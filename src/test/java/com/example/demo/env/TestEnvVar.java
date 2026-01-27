4fe3e86950b5e7e9ff0a9cc43b6164baf2a2dc57
TiavinaNath <hei.tiavina.3@gmail.com>
2026-01-27 14:58:20 +0300

    chore: test env vars (should work)

---
+++ src/test/java/com/example/demo/env/TestEnvVar.java
@@ -0,0 +1,26 @@
+package com.example.demo.env;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.example.demo.conf.FacadeIT;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+
+public class TestEnvVar extends FacadeIT {
+  @Value("${vola.api.url}")
+  private String volaApiUrl;
+
+  private String getVolaApiUrl = System.getenv("VOLA_API_URL");
+
+  @Test
+  void test_value() {
+    var value = "azertyuiop/123456789";
+    assertEquals(value, volaApiUrl);
+  }
+
+  @Test
+  void test_get_env() {
+    var value = "azertyuiop/123456789";
+    assertEquals(value, getVolaApiUrl);
+  }
+}
